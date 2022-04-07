b0ac38ba09cb6fa4cc1a2365421a998d9b97d567
Jose Daniel Grajales <84340043+DanielGrajales-U@users.noreply.github.com>
2022-04-07 11:02:35 -0500

    subimos todo listo
    mi primera base de una pag web

---
+++ desarrollo web/JS/app.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-btn").addEventListener("click",()=>{
+    //selectionamos la clase y el metodo toggle nos añadira una clase mas si no existe
+    document.querySelector(".nav-menu").classList.toggle("show");
+})
